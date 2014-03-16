58e86fba89a0259a6ff76623e0312d44f7a9a304
avandrey <aaron@vandrey.com>
2014-03-16 00:04:17 -0600

    Updating project with additional declarative bindings

--- twit-decision/public/javascripts/main.js
+++ twit-decision/public/javascripts/main.js
@@ -1,16 +1,22 @@
-/*global ko, $ */
-
+/*global ko */
 function ViewModel() {
     'use strict';
-	
-    var self = this;
-    // constants
-    self.RESULTS_START_HTML = 'and the winner is ... <strong>';
-    self.RESULTS_END_HTML = '</strong> ... with a score of ';
 
-	self.choices = [];
+    var self = this,
+    // constants
+        ERROR_TITLE = 'Error processing choices',
+        SUCCESS_TITLE = 'Decision Results',
+        RESULTS_START_HTML = 'and the winner is ... <strong>',
+        RESULTS_END_HTML = '</strong> ... with a score of ',
+        choices = [];
+
+    // utility
+    function getError (key) {
+        return (key && self.errors[key]) || self.errors.unknownError;
+    }
+
     self.errors = {
-        'sameInputError': 'Both choices are the same. Try again.',
+        'sameInputError': 'Both choices are the same. Try again.', 
         'requiredInputsError': 'You must enter a value for both choices.',
         'unknownError': 'An unknown error occurred.'
     };
@@ -18,18 +24,34 @@ function ViewModel() {
     // on screen text
     self.error = ko.observable('');
     self.results = ko.observable('');
-
+    self.messageTitle = ko.observable('');
+    
     // visual control
     self.isProcessing = ko.observable(false);
     self.hasResults = ko.observable(false);
+    self.shouldShowMessages = ko.computed(function(){
+        var returnValue = false;
 
-    // utility
-    self.getError = function (key) {
-        return (key && self.errors[key]) || self.errors.unknownError;
-    };
+        if (!self.isProcessing() && (self.hasResults() || self.error() > '')) {
+            returnValue = true;
+        }
 
+        return returnValue;
+    });
+    self.messageType = ko.computed(function(){
+        var returnValue = 'danger';
+
+        self.messageTitle(ERROR_TITLE);
+        if (self.hasResults()) {
+            returnValue = 'success';
+            self.messageTitle(SUCCESS_TITLE);
+        }
+
+        return returnValue;
+    });
+    
     // try again
-    self.tryAgain = function () {
+    self.tryAgain = function(){
         self.error('');
         self.isProcessing(false);
         self.hasResults(false);
@@ -37,36 +59,37 @@ function ViewModel() {
         self.inputOne('');
         self.inputTwo('');
     };
-
+    
     // form
     self.inputOne = ko.observable();
     self.inputTwo = ko.observable();
-    self.formSubmit = function () {
+    self.formSubmit = function(){
         // some error handling
-        if (!self.inputOne() || !self.inputTwo()) {
-            self.error(self.getError('requiredInputsError'));
-        } else if (self.inputOne() === self.inputTwo()) {
-            self.error(self.getError('sameInputError'));
+        if(!self.inputOne() || !self.inputTwo()){
+            self.error(getError('requiredInputsError'));
+        } else if(self.inputOne() === self.inputTwo()) {
+            self.error(getError('sameInputError'));
         } else {
-            self.choices.push(self.inputOne());
-            self.choices.push(self.inputTwo());
-            self.getDecision();
+            choices.push(self.inputOne());
+            choices.push(self.inputTwo());
+            getDecision();
             self.error('');
             self.isProcessing(true);
         }
     };
-
+    
     // posting
-    self.getDecision = function () {
+    function getDecision(){     
         // send values to server side for processing, wait for callback, getting AJAXy
-        $.post('/search', { 'choices': JSON.stringify(self.choices) }, function (data) {
+        $.post('/search', { 'choices': JSON.stringify(choices) }, function(data) {
+            choices.length = 0;
             var results = JSON.parse(data);
-
-            self.results(self.RESULTS_START_HTML + results.choice + self.RESULTS_END_HTML + results.score);
+            
+            self.results(RESULTS_START_HTML + results.choice + RESULTS_END_HTML + results.score);
             self.hasResults(true);
             self.isProcessing(false);
-        });
-    };
+        });  
+    }
 }
 
 ko.applyBindings(new ViewModel());
